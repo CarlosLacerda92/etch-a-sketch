b59c2caaeb8d6dfb08720045637aca2a24e17aa2
Carlos Lacerda <148892466+CarlosLacerda92@users.noreply.github.com>
2025-05-04 14:45:55 -0300

    feat: add "new grid" functionality
    The user is now able to create a brand new grid by clicking the "New
    grid" button on top of the screen. He or she will provide the dimensions
    of the new grid, e.g., by typing 5 a 5x5 grid will be created and so on.

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
-const container = document.getElementById('container');
+const container     = document.getElementById('container');
+const newGridButton = document.getElementById('newGrid');
+
+let numberOfSquares = 4;
+
+newGridButton.addEventListener('click', askNumberOfSquares);
 
 function createGrid() {
 
-    const numberOfRows    = 4;
-    const numberOfColumns = 4;
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
 
-    for(let i = 1; i <= numberOfRows; i++) {
+    for(let i = 0; i < numberOfSquares; i++) {
 
         const row = document.createElement('div');
         row.classList.add('row');
-        row.setAttribute('id', `row_${i}`);
+        row.setAttribute('id', `row_${i + 1}`);
 
-        for (let j = 1; j <= numberOfColumns; j++) {
+        for (let j = 1; j <= numberOfSquares; j++) {
             
             const column = document.createElement('div');
             column.classList.add('column');
-            column.setAttribute('id', `column_${i}_${j}`);
+            column.setAttribute('id', `column_${i + 1}_${j}`);
+            /* column.textContent = j + (numberOfSquares * i); */
 
             row.appendChild(column);
         }
 
-        container.appendChild(row);
+        grid.appendChild(row);
     }
+
+    container.appendChild(grid);
+}
+
+function removeGrid() {
+    container.querySelector('div.grid').remove();
+}
+
+function askNumberOfSquares() {
+
+    let newNumberOfSquares = 0;
+
+    while (newNumberOfSquares !== null && (!+newNumberOfSquares || isNaN(+newNumberOfSquares) || +newNumberOfSquares < 0 || +newNumberOfSquares > 100)) {
+        newNumberOfSquares = prompt("How many squares will the grid have on each side?\nMinimum: 0, maximum: 100.");
+    }
+
+    if (newNumberOfSquares === null) {
+        return;
+    }
+
+    numberOfSquares = +newNumberOfSquares;
+
+    removeGrid();
+    createGrid();
 }
 
 createGrid();
